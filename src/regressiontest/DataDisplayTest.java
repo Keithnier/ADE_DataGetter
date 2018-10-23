00d1edc53e9df0ee5990d7c57d1536b25d421893
keithnier <274176325@qq.com>
2018-10-23 08:48:52 +0800

    delete test

--- src/regressiontest/DataDisplayTest.java
+++
@@ -1,7 +0,0 @@
-package regressiontest;
-
-public class DataDisplayTest {
-    public static void main(String[] args) throws Exception {
-        DataGenerator.displayData();
-    }
-}
--- src/util/readData.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-
-import java.io.IOException;
-
-public interface readData {
-    String myreadline(String filepath);
-    void close()throws IOException;
-}
